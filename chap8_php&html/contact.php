4c7310871cffd8905f8a9484df10e31d9eaa9e70
Fiddjy <lagnela1996@gmail.com>
2024-03-09 11:59:29 +0100

    chapitre 8 php & html part 1

---
+++ chap8_php&html/contact.php
@@ -0,0 +1,11 @@
+<?php 
+$title = "Contact";
+$nav = "contact";
+require 'header.php'; 
+?>
+
+<h2>Nous contacter</h2>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus laboriosam adipisci illum, quas accusamus earum numquam debitis rem beatae ipsa excepturi. Itaque libero amet magni officiis error eveniet, ea magnam.</p>
+
+<?php require 'footer.php';?>
