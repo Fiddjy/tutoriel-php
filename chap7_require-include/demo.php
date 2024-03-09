0c19ffce2d6bdb9238a270aa0ba3cc8ae8801d36
Fiddjy <lagnela1996@gmail.com>
2024-03-09 10:07:14 +0100

    chapitre 7 : require et include

---
+++ chap7_require-include/demo.php
@@ -0,0 +1,7 @@
+<?php
+// https://www.php.net/manual/fr/function.include.php
+// https://www.php.net/manual/fr/function.require.php
+require 'functions.php';
+
+repondre_oui_non('Test');
+?>
---
+++ chap7_require-include/functions.php
@@ -0,0 +1,43 @@
+<?php
+function repondre_oui_non(string $phrase): bool {
+    while (true) {
+        $reponse = readline($phrase . " - (o)ui/(n)on");
+        if ($reponse === "o") {
+            return true;
+        } elseif ($reponse === "n") {
+            return false;
+        } 
+    }
+}
+
+function demander_creneau($phrase = 'Veuillez entrer votre creneau') {
+    echo $phrase . "\n";
+    while (true) {
+        $ouverture = (int)readline("Heure d'ouverture : ");
+        if ($ouverture >= 0 && $ouverture <= 23) {
+            break;
+        }
+    }
+    while (true) {
+        $fermeture = (int)readline("Heure de fermeture : ");
+        if ($fermeture >= 0 && $fermeture <= 23 && $fermeture > $ouverture) {
+            break;
+        }
+    }
+    return [$ouverture, $fermeture];
+}
+
+function demander_creneaux (string $phrase = "Veuillez entrer vos créneaux"): array {
+    $creneaux = [];
+    $continuer = true;
+    while ($continuer) {
+        $creneaux[] = demander_creneau();
+        $continuer = repondre_oui_non('Voulez vous continuer ?');
+    }
+    return $creneaux;
+}
+
+$creneaux = demander_creneaux('Entrez vos créneaux');
+var_dump($creneaux);
+
+?>
