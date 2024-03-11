54632cd5aef50283d71b6879fb8bdcb457bd2be4
Fiddjy <lagnela1996@gmail.com>
2024-03-11 10:46:29 +0100

    chap 9 création d'un formulaire

---
+++ chap9_formulaires/jeu.php
@@ -0,0 +1,37 @@
+<?php
+$aDeviner = 150;
+$erreur = null;
+$succes = null;
+$value = null;
+if (isset($_GET['chiffre'])) {
+    if ($_GET['chiffre'] > $aDeviner) {
+        $erreur = "Votre chiffre est trop grand";
+    } elseif ($_GET['chiffre'] < $aDeviner) {
+        $erreur = "Votre chiffre est trop petit";
+    } else {
+        $succes = "Bravo, vous avez deviné le chiffre <strong>$aDeviner</strong>";
+    }
+    $value = (int)$_GET['chiffre'];
+}
+require 'header.php';
+?>
+
+<?php if ($erreur) : ?>
+    <div class="alert alert-danger">
+        <?=$erreur?>
+    </div>
+<?php elseif ($succes): ?>
+    <div class="alert alert-success">
+        <?= $succes ?>
+    </div>
+<?php endif ?>
+
+<form action="/jeu.php" method="POST">
+    <div class="form-group">
+        <input type="number" class="form-control" name="chiffre" placeholder="Entre 0 et 1000" value="<?= $value ?>">
+    </div>
+        <button type="submit" class="btn btn-primary">Deviner</button>
+</form>
+
+
+<?php require 'footer.php'; ?>
