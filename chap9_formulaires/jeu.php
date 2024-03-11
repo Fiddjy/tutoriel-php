a256a94cf7f26127dfa215fa0e0e0b0ac4f3c561
Fiddjy <lagnela1996@gmail.com>
2024-03-11 11:49:25 +0100

    chapitre 9 formulaire : choisir un produit + calculer le prix

--- chap9_formulaires/jeu.php
+++ chap9_formulaires/jeu.php
@@ -1,17 +1,19 @@
 <?php
+// si GET = on a la requete dans l'url. Utile si on veut que le user partage son url
+// si post = on n'a pas la requete dans l'url. ex: une interface de connexion
 $aDeviner = 150;
 $erreur = null;
 $succes = null;
 $value = null;
-if (isset($_GET['chiffre'])) {
-    if ($_GET['chiffre'] > $aDeviner) {
+if (isset($_POST['chiffre'])) {
+    $value = (int)$_POST['chiffre'];
+    if ($value > $aDeviner) {
         $erreur = "Votre chiffre est trop grand";
-    } elseif ($_GET['chiffre'] < $aDeviner) {
+    } elseif ($value < $aDeviner) {
         $erreur = "Votre chiffre est trop petit";
     } else {
         $succes = "Bravo, vous avez deviné le chiffre <strong>$aDeviner</strong>";
     }
-    $value = (int)$_GET['chiffre'];
 }
 require 'header.php';
 ?>
@@ -30,8 +32,17 @@ require 'header.php';
     <div class="form-group">
         <input type="number" class="form-control" name="chiffre" placeholder="Entre 0 et 1000" value="<?= $value ?>">
     </div>
-        <button type="submit" class="btn btn-primary">Deviner</button>
+    <button type="submit" class="btn btn-primary">Deviner</button>
 </form>
 
+<h2>$_GET</h2>
+<pre>
+    <?php var_dump($_GET); ?>
+</pre>
+
+<h2>$_POST</h2>
+<pre>
+    <?php var_dump($_POST); ?>
+</pre>
 
 <?php require 'footer.php'; ?>
