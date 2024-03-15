b3f0821bcfc83077e3220cc5aecc635791bba3f3
Fiddjy <lagnela1996@gmail.com>
2024-03-15 18:12:32 +0100

    chapitre 12 : création d'une neweletter + ajout sur le footer, sauf sur la page de la newsletter

--- chap12_ecritureDeFichiers/elements/footer.php
+++ chap12_ecritureDeFichiers/elements/footer.php
@@ -1,15 +1,31 @@
 </main><!-- /.container -->
 
-<div class="row">
-    <div class="col-md-4"></div>
-    <div class="col-md-4"></div>
-    <div class="col-md-4">
-        <h5>Navigation</h5>
-        <ul class="list-unstyled text-small">
-            <?= nav_menu('') ?>
-        </ul>
+<footer>
+    <hr>
+    <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+            <?php if ($title !== "Newsletter") :?>
+                <form action="/newsletter.php" method="POST" class="row g-3 needs-validation" novalidate="novalidate">
+                    <div class="">
+                        <div class="input-group has-validation">
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Entrer votre e-mail" required="required">
+                        </div>
+                    </div>
+                    <div class="">
+                        <button class="btn btn-primary" type="submit">S'inscrire</button>
+                    </div>
+                </form>
+            <?php endif ?>
+        </div>
+        <div class="col-md-4">
+            <h5>Navigation</h5>
+            <ul class="list-unstyled text-small">
+                <?= nav_menu('') ?>
+            </ul>
+        </div>
     </div>
-</div>
+</footer>
 
 <!-- Bootstrap core JavaScript
 ================================================== -->
